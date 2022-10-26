5873a272de92d72827271a9f131fc6fd84777e9e
Laurenz Dilba <laurenz.dilba@hotmail.de>
2022-10-26 16:42:49 +0200

    Added http Response after Receive

--- test_reciever.py
+++ test_reciever.py
@@ -1,17 +1,24 @@
+import json
+
 import pika
+import requests
 
 
-def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+def callback(ch, method, properties, body: bytes):
+    received = json.loads(body.decode('utf-8'))
+
+    print(received)
+    requests.post("http://localhost:80/receive",
+                  json={"uid": received["uid"], "service": received["service"], "message": "Hier ist die Response :)"})
 
 
 if __name__ == '__main__':
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='service')
+    channel.queue_declare(queue='text-similarity')
 
-    channel.basic_consume(queue='service',
+    channel.basic_consume(queue='text-similarity',
                           auto_ack=True,
                           on_message_callback=callback)
 
--- test_sender.py
+++ test_sender.py
@@ -4,12 +4,12 @@ if __name__ == '__main__':
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='service')
+    channel.queue_declare(queue='text-similarity-response')
 
     channel.basic_publish(exchange='',
-                          routing_key='service',
-                          body=b'hi :3')
+                          routing_key='text-similarity-response',
+                          body=f'{{"uid": "2784f7fb-cd3d-41c8-8949-ba4aa8d53067", "service": "text-similarity-response",'
+                               f'"response": "das ist eine test response"}}'.encode('utf-8'))
 
     print(" [x] Sent 'Hello World!'")
     connection.close()
-
