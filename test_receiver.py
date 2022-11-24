c5edf55bc511feb8cff65f362893ed8e4f251311
Laurenz Dilba <laurenz.dilba@hotmail.de>
2022-11-24 12:02:47 +0100

    Changed HTTP Response to AMQP Response

--- test_receiver.py
+++ test_receiver.py
@@ -5,15 +5,23 @@ import requests
 import os
 
 
+def respond(uid, service):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host, 5672, '/', credentials))
+    channel = connection.channel()
+    channel.queue_declare(queue='response-text-similarity')
+    message = '{"title": "How to stop an interval on an Observable in RxJS", "link": "https://stackoverflow.com/questions/46963486/how-to-stop-an-interval-on-an-observable-in-rxjs", "content": "I have a specific situation where I\'m using an RxJS interval, but at any given moment I may need to stop that interval. I assumed there was something easy like a cancel() or stop(). Similar to clearTimeout. This is possible to stop an interval once it\'s going? If not, what would be another approach."}'
+
+    channel.basic_publish(exchange='',
+                          routing_key='response-text-similarity',
+                          body=f'{{"uid": "{uid}", "service": "{service}", "message": {message}}}'.encode('utf-8'))
+    channel.close()
+    connection.close()
+
+
 def callback(ch, method, properties, body: bytes):
     received = json.loads(body.decode('utf-8'))
     print(received)
-    cookies = {}
-
-    url = os.environ.get('FLASK_URL')
-    r = requests.post(url + "/receive", cookies=cookies,
-                      json={"uid": received["uid"], "service": received["service"],
-                            "message": "Why you should never center a div."})
+    respond(received["uid"], received["service"])
 
 
 if __name__ == '__main__':
